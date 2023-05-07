e0dc0c554234704e38e28e95e1a0a5f053792185
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-07 22:57:05 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientList = document.querySelector('#ingredients');
+
+const foodList = ingredients.map(ingredient => {
+const ingredientEl = document.createElement('li');
+ingredientEl.classList.add('item');
+ingredientEl.textContent = ingredient;
+
+  return ingredientEl;
+
+});
+
+console.log(foodList);
+
+ingredientList.append(...foodList);
+
