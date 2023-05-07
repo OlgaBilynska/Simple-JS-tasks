f38f37b7f1e93336c9c998ef4905b855e829a500
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-07 21:53:23 +0300

    changing task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,23 @@
-const categoryList = document.querySelector('ul#categories');
-const allCategories = categoryList.querySelectorAll('li.item');
-console.log("Number of categories:", allCategories.length);
+const categoryList = document.querySelector('#categories');
+// const allCategories = categoryList.querySelectorAll('li.item');
+console.log("Number of categories:", categoryList.children.length);
 
-const categoryNames = [];
-allCategories.forEach((category, i) => {
-    const header = category.querySelector('h2');
-    const headerName = header.textContent;
-    categoryNames.push(headerName);
-    console.log("Category:", categoryNames[i]);
-    const categoryChildrenArray = category.querySelectorAll('li');
-    console.log("Elements:", categoryChildrenArray.length);
-})
+
+[...categoryList.children].forEach((category) => {
+    const headerEl = category.firstElementChild.textContent;
+    console.log("Category:", headerEl);
+    const elementsArr = category.lastElementChild.children;
+    console.log("Elements:", elementsArr.length);
+}
+)
+
+// const categoryNames = [];
+// allCategories.forEach((category, i) => {
+//     const header = category.querySelector('h2');
+//     const headerName = header.textContent;
+//     categoryNames.push(headerName);
+//     console.log("Category:", categoryNames[i]);
+//     const categoryChildrenArray = category.querySelectorAll('li');
+//     console.log("Elements:", categoryChildrenArray.length);
+// })
 
