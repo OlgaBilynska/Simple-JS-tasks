a5911a709c6561aba18049c8628e992106c4347e
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-08 12:57:47 +0300

    task 4 ready

--- js/task-04.js
+++ js/task-04.js
@@ -1,38 +1,31 @@
 let counterValue = 0;
 
 const counterEl = document.querySelector('#counter');
-console.log(counterEl);
-
 const decrementBtn = counterEl.querySelector('[data-action="decrement"]');
-console.log(decrementBtn);
-
 const incrementBtn = counterEl.querySelector('[data-action="increment"]')
-console.log(incrementBtn);
 
 let counterNumberEl = document.querySelector('#value');
-let valueEl = counterNumberEl.textContent;
+
+const updateValueEl = () => {
+    counterNumberEl.textContent = counterValue;
+}
 
 const decreaseValue = () => {
   counterValue -= 1;
-    console.log(counterValue);
+    // console.log(counterValue);
+    updateValueEl();
     return counterValue;
 };
 
 const increaseValue = () => {
     counterValue += 1;
-    console.log(counterValue);
+    // console.log(counterValue);
+    updateValueEl();
     return counterValue;
 }
 
 const updateDecrementValue = decrementBtn.addEventListener('click', decreaseValue);
 const updateIncrementValue = incrementBtn.addEventListener('click', increaseValue);
 
-console.log(counterValue);
 
-const updateValueEl = () => {
-    valueEl = counterValue;
-    console.log(valueEl);
-    return counterNumberEl;
-}
 
-updateValueEl();
