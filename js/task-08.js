50261769008315743a54c382607a64fb3367597f
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-09 19:38:43 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector('.login-form');
+console.log(formEl);
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+        const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+        alert('Fill in all the fields');
+        return;
+    }
+
+    const formData = { email, password };
+    console.log(formData);
+
+    formEl.reset();
+}
+
+
+
 
