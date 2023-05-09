2d001649b699db550e9166017a8fedd3e18f7999
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-09 20:08:07 +0300

    task 9

--- js/task-08.js
+++ js/task-08.js
@@ -6,7 +6,7 @@ formEl.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
     event.preventDefault();
 
-        const formElements = event.currentTarget.elements;
+    const formElements = event.currentTarget.elements;
     const email = formElements.email.value;
     const password = formElements.password.value;
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
+const buttonEl = document.querySelector('.change-color');
+console.log(buttonEl);
+const colorNameEl = document.querySelector('.color');
+console.log(colorNameEl);
+
+buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = bodyEl.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
